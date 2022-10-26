619a4839fa63ca2bf5ca8b95b3ad30bfad20c551
troscoeftnt <91550059+troscoeftnt@users.noreply.github.com>
2022-10-26 15:11:45 -0400

    Update main.tf
    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "sandbox-aft@ftnt.cloud"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "troscoe@fortinet.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
